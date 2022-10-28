7e723c8984bbb8ceb1291d5b2b167d0a8c685730
ArugaZ <arugaastri@gmail.com>
2022-10-29 01:54:40 +0700

    page: change favicon

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,8 @@ const Document = () => {
           href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/images/favicon/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/images/favicon/favicon.ico" type="image/x-icon" />
       </Head>
       <body>
         <Main />
