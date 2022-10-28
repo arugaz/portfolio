e594f9cc1b69dc9b86e27a1c6b686333d0da0146
ArugaZ <arugaastri@gmail.com>
2022-10-29 04:44:06 +0700

    page: add about page

--- data/pages.js
+++ data/pages.js
@@ -5,4 +5,10 @@ export const pages = [
     desc: 'ArugaZ is one of the developers in Indonesia. My name is Arga Astri Bimantara, feel free to say hi.',
     image: 'profile.jpg',
   },
+  {
+    path: '/about',
+    title: 'About',
+    desc: 'My name is Arga Astri Bimantara, you can call me Aruga. I was born in Jakarta, Indonesia.',
+    image: 'profile.jpg',
+  },
 ];
