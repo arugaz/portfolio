3f9f6e00fd5c3a8d9b9b36c4a16c4b58996dd22f
ArugaZ <arugaastri@gmail.com>
2022-10-29 16:49:21 +0700

    next: edit configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  compress: false,
+  generateBuildId: async () => {
+    return 'arugaz-id';
+  },
+  poweredByHeader: false,
   swcMinify: true,
 };
 
