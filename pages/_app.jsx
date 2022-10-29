c70334b28cfc947b53d56c5bed4a2f2aeba3b8d6
ArugaZ <arugaastri@gmail.com>
2022-10-30 05:16:28 +0700

    page: edit load time

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -28,16 +28,6 @@ const MyApp = ({ Component, pageProps }) => {
     };
   }, []);
 
-  useEffect(() => {
-    const head = document.getElementsByTagName('head')[0];
-    const scriptElement = document.createElement('script');
-    scriptElement.type = 'text/javascript';
-    scriptElement.async = true;
-    scriptElement.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_ID}`;
-    scriptElement.crossOrigin = 'anonymous';
-    head.appendChild(scriptElement);
-  }, []);
-
   return (
     <Layout>
       {loading ? (
