7a08f67954a3d69820138de57a0ab3abd6cad2ef
unknown <skyubusiness@gmail.com>
2023-06-23 07:12:57 +0700

    Ini adalah function iklan, iklan menggunakan web adsterra

---
+++ pages/Adsterra.tsx
@@ -0,0 +1,76 @@
+export function AdsterraScript728() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<script type="text/javascript">
+            atOptions = {
+              'key' : '902e0534872098b95a22a4ede849bd7b',
+              'format' : 'iframe',
+              'height' : 90,
+              'width' : 728,
+              'params' : {}
+            };
+            document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://tacticschangebabysitting.com/902e0534872098b95a22a4ede849bd7b/invoke.js"></scr' + 'ipt>');
+          </script>`,
+      }}
+    />
+  ); //ADS 728 UPDATED
+}
+
+export function AdsterraScript468() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<script type="text/javascript">
+              atOptions = {
+                'key' : 'c7bf695351d3436aa73ada26c76c7e42',
+                'format' : 'iframe',
+                'height' : 60,
+                'width' : 468,
+                'params' : {}
+              };
+              document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://tacticschangebabysitting.com/c7bf695351d3436aa73ada26c76c7e42/invoke.js"></scr' + 'ipt>');
+            </script>`,
+      }}
+    />
+  );
+} //updated
+
+export function AdsterraScript300() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<script type="text/javascript">
+              atOptions = {
+                'key' : 'd48d8a3bd9c325de29639d461b8ed27c',
+                'format' : 'iframe',
+                'height' : 250,
+                'width' : 300,
+                'params' : {}
+              };
+              document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://tacticschangebabysitting.com/d48d8a3bd9c325de29639d461b8ed27c/invoke.js"></scr' + 'ipt>');
+            </script>`,
+      }}
+    />
+  );
+} //ADS 300 UPDATED
+
+export function AdsterraScriptPopunder() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<script type='text/javascript' src='//tacticschangebabysitting.com/c3/a5/24/c3a524fdf8af81aa1ddcf4311b6772f1.js'></script>`,
+      }}
+    />
+  );
+} //POPUNDER UPDATED
+
+export function AdsterraScriptSocial() {
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `<script type='text/javascript' src='//tacticschangebabysitting.com/25/51/97/2551972e3dd95ce3f01e77faaa6b2b5e.js'></script>`,
+      }}
+    />
+  );
+} //SOCIAL UDATED
