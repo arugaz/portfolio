641aa3edd89f5ffd1c1443dc99715adc4b0ff5ab
unknown <skyubusiness@gmail.com>
2023-06-23 07:12:02 +0700

    ini adalah laman download, bisa menambahkan laman download pubg/apk mod

---
+++ data/pubg_conf.ts
@@ -0,0 +1,43 @@
+export const pubg = [
+  {
+    title: "SUPER PERFZ V3 Series",
+    download: "https://sfile.mobi/1L9Ctx8AWMS7",
+    version: "2.5",
+  },
+  {
+    title: "SUPER PERFZ V5 Series",
+    download: "",
+    version: "2.6",
+  },
+  {
+    title: "SUPER PERFZ V6 Series",
+    download: "https://youtube.com/@SkyUniverseNanz",
+    version: "coming soon",
+  },
+];
+
+export const apkmod = [
+  {
+    title: "MT Manager",
+    download: "https://sfile.mobi/FtJfdWNlWE7",
+    version: "latest",
+  },
+  {
+    title: "JM Tools",
+    download: "https://sfile.mobi/CCs3wXvOA87",
+    version: "latest",
+  },
+  {
+    title: "PS CC 2019",
+    download: "https://sfile.mobi/CCs3wXvOA87",
+    version: "2019",
+  },
+];
+
+export const magisk = [
+  {
+    title: "Tweak Peformance",
+    download: "",
+    version: "latest"
+  }
+]
